9f9d22ce73762cd1875e43fd187233260d181629
gimunkwon <gimon71@gmail.com>
2026-04-30 11:14:53 +0900

    [add] 플레이어 컨트롤러 IMC 매핑 로직 초기화

--- Source/NBC_Client_Dowork_03/Public/GameActor/Controller/MyPlayerController.h
+++ Source/NBC_Client_Dowork_03/Public/GameActor/Controller/MyPlayerController.h
@@ -4,8 +4,22 @@
 #include "GameFramework/PlayerController.h"
 #include "MyPlayerController.generated.h"
 
+class UInputAction;
+class UInputMappingContext;
+
 UCLASS()
 class NBC_CLIENT_DOWORK_03_API AMyPlayerController : public APlayerController
 {
 	GENERATED_BODY()
+public:
+	AMyPlayerController();
+protected:
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="Input")
+	TObjectPtr<UInputMappingContext> IMC_Default;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="Input")
+	TObjectPtr<UInputAction> IA_Move;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="Input")
+	TObjectPtr<UInputAction> IA_Rotate;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="Input")
+	TObjectPtr<UInputAction> IA_Attack;
 };
