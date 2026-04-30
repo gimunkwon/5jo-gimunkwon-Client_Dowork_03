facc81b03a84a210bc04a71203273c2026ca4ce6
gimunkwon <gimon71@gmail.com>
2026-04-30 09:09:19 +0900

    [add] 언리얼 기초 빌드 파일 추가

---
+++ Source/NBC_Client_Dowork_03/NBC_Client_Dowork_03.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "NBC_Client_Dowork_03.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, NBC_Client_Dowork_03, "NBC_Client_Dowork_03" );
