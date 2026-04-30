b1a9ae731aba12f883f429f793bf79e639f18248
gimunkwon <gimon71@gmail.com>
2026-04-30 11:57:57 +0900

    [add] 플레이어 AnimInst추가

---
+++ Source/NBC_Client_Dowork_03/Private/GameActor/Animations/MyPlayerAnimInst.cpp
@@ -0,0 +1,4 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "NBC_Client_Dowork_03/Public/GameActor/Animations/MyPlayerAnimInst.h"
---
+++ Source/NBC_Client_Dowork_03/Public/GameActor/Animations/MyPlayerAnimInst.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "MyPlayerAnimInst.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NBC_CLIENT_DOWORK_03_API UMyPlayerAnimInst : public UAnimInstance
+{
+	GENERATED_BODY()
+};
