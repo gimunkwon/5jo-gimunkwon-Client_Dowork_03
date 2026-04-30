b54649ded3fec9e5c73c18161e09b11a705c85b8
gimunkwon <gimon71@gmail.com>
2026-04-30 12:37:23 +0900

    [add] 블랜드 스페이스 스피드 값을 위한 현재 애님 인스턴스의 오너의 GetVelocity값을 미리 캐싱해서 사용

--- Source/NBC_Client_Dowork_03/Private/GameActor/Animations/MyPlayerAnimInst.cpp
+++ Source/NBC_Client_Dowork_03/Private/GameActor/Animations/MyPlayerAnimInst.cpp
@@ -1,4 +1,21 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "NBC_Client_Dowork_03/Public/GameActor/Animations/MyPlayerAnimInst.h"
+
+#include "NBC_Client_Dowork_03/Public/GameActor/MyPlayer.h"
+
+void UMyPlayerAnimInst::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+	
+	MyOwnerInst = Cast<AMyPlayer>(TryGetPawnOwner());
+}
+
+void UMyPlayerAnimInst::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+	
+	if (MyOwnerInst)
+	{
+		FVector Velocity = MyOwnerInst->GetVelocity();
+		GroundSpeed = Velocity.Size2D();
+	}
+}
--- Source/NBC_Client_Dowork_03/Public/GameActor/Animations/MyPlayerAnimInst.h
+++ Source/NBC_Client_Dowork_03/Public/GameActor/Animations/MyPlayerAnimInst.h
@@ -1,16 +1,21 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "Animation/AnimInstance.h"
 #include "MyPlayerAnimInst.generated.h"
 
-/**
- * 
- */
+class AMyPlayer;
+
 UCLASS()
 class NBC_CLIENT_DOWORK_03_API UMyPlayerAnimInst : public UAnimInstance
 {
 	GENERATED_BODY()
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+protected:
+	UPROPERTY()
+	TObjectPtr<AMyPlayer> MyOwnerInst;
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly,Category="Speed")
+	float GroundSpeed;
 };
