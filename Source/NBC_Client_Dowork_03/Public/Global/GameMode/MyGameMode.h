a47d0cbb2cc69e8046e02c1ffce38f97c364fc2d
gimunkwon <gimon71@gmail.com>
2026-04-30 10:00:17 +0900

    [add] GameMode클래스 추가

---
+++ Source/NBC_Client_Dowork_03/Public/Global/GameMode/MyGameMode.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "MyGameMode.generated.h"
+
+UCLASS()
+class NBC_CLIENT_DOWORK_03_API AMyGameMode : public AGameMode
+{
+	GENERATED_BODY()
+};
