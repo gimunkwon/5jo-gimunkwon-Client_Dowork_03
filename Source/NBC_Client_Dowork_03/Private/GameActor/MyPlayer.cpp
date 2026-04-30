b5912aac12d1109d6a376e0013c0ab1c52256002
gimunkwon <gimon71@gmail.com>
2026-04-30 10:29:33 +0900

    [add] 플레이어 카메라 컴포넌트 생성자 초기화

--- Source/NBC_Client_Dowork_03/Private/GameActor/MyPlayer.cpp
+++ Source/NBC_Client_Dowork_03/Private/GameActor/MyPlayer.cpp
@@ -1,18 +1,22 @@
 #include "NBC_Client_Dowork_03/Public/GameActor/MyPlayer.h"
 
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
 
 
 AMyPlayer::AMyPlayer()
 {
-	
 	PrimaryActorTick.bCanEverTick = true;
+	
+	CameraComp = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComponent"));
+	CameraComp->SetupAttachment(RootComponent);
+	CameraComp->bUsePawnControlRotation = true;
 }
 
 
 void AMyPlayer::BeginPlay()
 {
 	Super::BeginPlay();
-	
 }
 
 
--- Source/NBC_Client_Dowork_03/Public/GameActor/MyPlayer.h
+++ Source/NBC_Client_Dowork_03/Public/GameActor/MyPlayer.h
@@ -4,23 +4,21 @@
 #include "GameFramework/Character.h"
 #include "MyPlayer.generated.h"
 
+class UCameraComponent;
+class USpringArmComponent;
+
 UCLASS()
 class NBC_CLIENT_DOWORK_03_API AMyPlayer : public ACharacter
 {
 	GENERATED_BODY()
 
 public:
-	
 	AMyPlayer();
-
 protected:
-	
 	virtual void BeginPlay() override;
-
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly,Category="Camera")
+	TObjectPtr<UCameraComponent> CameraComp;
 public:
-	
 	virtual void Tick(float DeltaTime) override;
-
-	
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 };
